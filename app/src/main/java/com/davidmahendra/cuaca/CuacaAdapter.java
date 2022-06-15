75a9ac43e2df3f7d6713dd47bcb4253d80acb72e
tolepganteng <93376346+tolepganteng@users.noreply.github.com>
2022-06-15 18:16:25 +0700

    update
    hehe

--- app/src/main/java/com/davidmahendra/cuaca/CuacaAdapter.java
+++ app/src/main/java/com/davidmahendra/cuaca/CuacaAdapter.java
@@ -1,11 +1,115 @@
 package com.davidmahendra.cuaca;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.lang.invoke.MethodHandles;
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class CuacaAdapter extends RecyclerView.Adapter<CuacaViewHolder> {
     private List<ListModel> listModelList;
     private RootModel rm;
-    
+
+    public CuacaAdapter(RootModel rm) {
+        this.rm = rm;
+
+        listModelList = rm.getListModelList();
+    }
+
+    @NonNull
+    @Override
+    public CuacaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.list_cuaca, parent, false);
+        return new CuacaViewHolder(view);
+    }
+
+    private double toCelcius(double kelvin) { return kelvin - 272.15; }
+
+    public String formatNumber (double number, String format) {
+        DecimalFormat decimalFormat = new DecimalFormat(format);
+        return decimalFormat.format(number);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CuacaViewHolder holder, int position) {
+        ListModel lm = listModelList.get(position);
+        WeatherModel wm = lm.getWeatherModelList().get(0);
+        MainModel mm = lm.getMainModel();
+
+        String suhu = formatNumber(toCelcius(mm.getTemp_min()), "###.##") + "°C - " +
+                formatNumber(toCelcius(mm.getTemp_max()), "###.##") + "C";
+
+        switch (wm.getIcon()) {
+            case "01d":
+                holder.cuacaImageView.setImageResource(R.mipmap.ic_01d);
+                break;
+
+            case "01n":
+                holder.cuacaImageView.setImageResource(R.mipmap.ic_01n);
+                break;
+
+            case "02d":
+                holder.cuacaImageView.setImageResource(R.mipmap.ic_02d);
+                break;
+
+            case "02n":
+                holder.cuacaImageView.setImageResource(R.mipmap.ic_02n);
+                break;
+
+            case "03d":
+                holder.cuacaImageView.setImageResource(R.mipmap.ic_03d);
+                break;
+
+            case "03n":
+                holder.cuacaImageView.setImageResource(R.mipmap.ic_03n);
+                break;
+
+            case "04d":
+                holder.cuacaImageView.setImageResource(R.mipmap.ic_04d);
+                break;
+
+            case "04n":
+                holder.cuacaImageView.setImageResource(R.mipmap.ic_04n);
+                break;
+
+            case "09d":
+                holder.cuacaImageView.setImageResource(R.mipmap.ic_09d);
+                break;
+
+            case "09n":
+                holder.cuacaImageView.setImageResource(R.mipmap.ic_09d);
+                break;
+
+            case "10d":
+                holder.cuacaImageView.setImageResource(R.mipmap.ic_10d);
+                break;
+
+            case "10n":
+                holder.cuacaImageView.setImageResource(R.mipmap.ic_10n);
+                break;
+
+            case "11d":
+                holder.cuacaImageView.setImageResource(R.mipmap.ic_11d);
+                break;
+
+            case "11n":
+                holder.cuacaImageView.setImageResource(R.mipmap.ic_11n);
+                break;
+        }
+
+        holder.namaTextView.setText(wm.getMain());
+        holder.deskrisiTextView.setText(wm.getDescription());
+        holder.tglWaktuTextView.setText(lm.getDt_txt());
+        holder.suhuTextView.setText(suhu);
+    }
+
+    @Override
+    public int getItemCount() { return (listModelList != null) ? listModelList.size() : 0; }
+
 }
--- app/src/main/java/com/davidmahendra/cuaca/MainActivity.java
+++ app/src/main/java/com/davidmahendra/cuaca/MainActivity.java
@@ -1,14 +1,73 @@
 package com.davidmahendra.cuaca;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private RecyclerView _recyclerView1;
+    private SwipeRefreshLayout _swipeRefreshLayout1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _recyclerView1 = (RecyclerView)findViewById(R.id.recycleView1);
+        _swipeRefreshLayout1 = findViewById(R.id.swip);
+
+        initRecyclerView();
+        initSwipeRefreshlayout();
+    }
+
+    private void initSwipeRefreshlayout()
+    {
+        _swipeRefreshLayout1.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener()
+        {
+            @Override
+            public void onRefresh()
+            {
+                initRecylerView1();
+                _swipeRefreshLayout1.setRefreshing(false);
+            }
+        });
+    }
+
+    private void initRecylerView1() {
+        String url = "https://api.openweathermap.org/data/2.5/forecast?id=1630789&appid=3fcbc6b19cef75132890e7e9b8ced300";
+        AsyncHttpClient ahc = new AsyncHttpClient();
+
+        ahc.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                //Log.d("*tw*", new String(responseBody));
+                Gson gson = new Gson();
+                RootModel rm = gson.fromJson(new String(responseBody), RootModel.class);
+                //Log.d("*tw*, rm.getListModelList().get(0).getDt_txt());
+                RecyclerView.LayoutManager lm = new LinearLayoutManager( MainActivity.this);
+                CuacaAdapter ca = new CuacaAdapter(rm);
+
+                _recyclerView1.setLayoutManager(lm);
+                _recyclerView1.setAdapter(ca);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG.show();
+
+            }
+        });
     }
 }
